495a73d553841a742cbacb338584b039dc38f605
Andrew Mordecai <am@armord.net>
2011-03-02 04:05:31 +0000

    Updated localization files

--- Locales/Locale-ruRU.lua
+++ Locales/Locale-ruRU.lua
@@ -13,128 +13,130 @@
 
 if GetLocale() ~= "ruRU" or select(2, UnitClass("player")) ~= "SHAMAN" then return end
 
-local L = { }
-local ADDON_NAME, namespace = ...
-namespace.L = L
+local _, namespace = ...
+namespace.L = {
 
 ------------------------------------------------------------------------
 -- These strings are displayed when shields expire.
 
-L["%s faded!"] = "%s спал!"
-L["%s faded from %s!"] = "%s спал с %s!"
-L["YOU"] = "ВАС"
+	["%s faded!"] = "%s спал!",
+	["%s faded from %s!"] = "%s спал с %s!",
+	["YOU"] = "ВАС",
 
 ------------------------------------------------------------------------
 -- These strings are displayed in the configuration GUI.
 
-L["Click for options."] = "Клик - открывает окно настроек."
+	["Click for options."] = "Клик - открывает окно настроек.",
 
--- L["ShieldsUp is a monitor for your shaman shields. Use these settings to configure the addon's appearance and behavior."] = ""
+--	["ShieldsUp is a monitor for your shaman shields. Use these settings to configure the addon's appearance and behavior."] = "",
 
-L["Horizontal Position"] = "Горизонтальная позиция"
-L["Set the horizontal distance from the center of the screen to place the display."] = "Установить горизонтальную позицию относительно центра экрана."
+	["Horizontal Position"] = "Горизонтальная позиция",
+	["Set the horizontal distance from the center of the screen to place the display."] = "Установить горизонтальную позицию относительно центра экрана.",
 
-L["Vertical Position"] = "Вертикальная позиция"
-L["Set the vertical distance from the center of the screen to place the display."] = "Установить вертикальную позицию относительно центра экрана."
+	["Vertical Position"] = "Вертикальная позиция",
+	["Set the vertical distance from the center of the screen to place the display."] = "Установить вертикальную позицию относительно центра экрана.",
 
-L["Horizontal Padding"] = "Горизонтальный отступ"
-L["Set the horizontal space between the charge counters."] = "Установить горизонтальный отступ между элементами текста."
+	["Horizontal Padding"] = "Горизонтальный отступ",
+	["Set the horizontal space between the charge counters."] = "Установить горизонтальный отступ между элементами текста.",
 
-L["Vertical Padding"] = "Вертикальный отступ"
-L["Set the vertical space between the target name and charge counters."] = "Установить вертикальный отступ между элементами текста."
+	["Vertical Padding"] = "Вертикальный отступ",
+	["Set the vertical space between the target name and charge counters."] = "Установить вертикальный отступ между элементами текста.",
 
-L["Opacity"] = "Прозрачность"
-L["Set the opacity level for the display."] = "Установить уровень прозрачности."
+	["Opacity"] = "Прозрачность",
+	["Set the opacity level for the display."] = "Установить уровень прозрачности.",
 
--- L["Overwrite Alert"] = ""
--- L["Print a message in the chat frame alerting you who overwrites your %s."] = ""
-
-------------------------------------------------------------------------
-
-L["Font Face"] = "Шрифт"
-L["Set the font face to use for the display text."] = "Установить размер шрифта для имени."
-
-L["Outline"] = "Обрисовка"
--- L["Select an outline width for the display text."] = ""
--- L["None"] = ""
--- L["Thin"] = ""
--- L["Thick"] = ""
-
--- L["Shadow"] = ""
--- L["Add a drop shadow effect to the display text."] = ""
-
-L["Counter Size"] = "Размер числа"
-L["Set the text size for the charge counters."] = "Установить размер шрифта для счетчиков."
-
-L["Name Size"] = "Размер имени"
-L["Set the text size for the target name."] = "Установить размер шрифта для имени."
+--	["Overwrite Alert"] = "",
+--	["Print a message in the chat frame alerting you who overwrites your %s."] = "",
 
 ------------------------------------------------------------------------
 
-L["Colors"] = "Цвета"
-L["Set the color for the %s charge counter."] = "Использовать этот цвет для счетчика зарядов %s."
+	["Font Face"] = "Шрифт",
+	["Set the font face to use for the display text."] = "Установить размер шрифта для имени.",
 
-L["Active"] = "Обычные"
-L["Set the color for the target name while your %s is active."] = "Использовать этот цвет для цели, на которой %s."
+	["Outline"] = "Обрисовка",
+--	["Select an outline width for the display text."] = "",
+--	["None"] = "",
+--	["Thin"] = "",
+--	["Thick"] = "",
 
-L["Overwritten"] = "Перебит"
-L["Set the color for the target name when your %s has been overwritten."] = "Использовать этот цвет для цели, на которой %s, в случае если чужой щит перебил ваш на этой цели."
+--	["Shadow"] = "",
+--	["Add a drop shadow effect to the display text."] = "",
 
-L["Inactive"] = "Предупреждение"
-L["Set the color for expired, dispelled, or otherwise inactive shields."] = "Использовать этот цвет, когда счечик зарядов на нуле."
+	["Counter Size"] = "Размер числа",
+	["Set the text size for the charge counters."] = "Установить размер шрифта для счетчиков.",
 
-L["Colorblind Mode"] = USE_COLORBLIND_MODE -- Leave this as-is unless there is something wrong with Blizzard's translation in your locale
--- L["Add asterisks around the target name when your %s has been overwritten, in addition to changing the color."] = ""
-
-------------------------------------------------------------------------
-
-L["Alerts"] = "Предупреждения"
--- L["Use these settings to configure how ShieldsUp alerts you when a shield expires or is removed."] = ""
-
-L["Text Alert"] = "Текст"
-L["Show a text message when %s expires."] = "Показывать предупреждение, когда спадает %s."
-
-L["Sound Alert"] = "Звук"
-L["Play a sound when %s expires."] = "Проигрывать звук, когда спадает %s."
-
-L["Sound File"] = "Звуковой файл"
--- L["Select the sound to play when %s expires."] = ""
-
--- L["Text Output"] = ""
+	["Name Size"] = "Размер имени",
+	["Set the text size for the target name."] = "Установить размер шрифта для имени.",
 
 ------------------------------------------------------------------------
 
-L["Visibility"] = "Видимость"
-L["Use these settings to control when the ShieldsUp display should be shown or hidden."] = "Настройка отображения в зависимости от следующих условий."
+	["Colors"] = "Цвета",
+	["Set the color for the %s charge counter."] = "Использовать этот цвет для счетчика зарядов %s.",
 
-L["Group Size"] = "Размер группы"
-L["Solo"] = "Соло"
-L["Show while not in a group."] = "Показывать, когда не в группе."
-L["Party"] = "Группа"
-L["Show while in a 5-man party."] = "Показывать, когда в группе из 5 человек."
-L["Raid"] = "Рейд"
-L["Show while in a raid group."] = "Показывать, когда в рейд.е"
+	["Active"] = "Обычные",
+	["Set the color for the target name while your %s is active."] = "Использовать этот цвет для цели, на которой %s.",
 
-L["Zone Type"] = "Тип зоны"
-L["World"] = "Мир"
-L["Show while in the world"] = "Показывать, когда в мире."
-L["Party Dungeon"] = "Подземелье"
-L["Show while in a 5-man dungeon"] = "Показывать, когда в подземелье на 5 человек."
-L["Raid Dungeon"] = "Инстанс-рейд"
-L["Show while in an raid dungeon"] = "Показывать, когда в инстансе-рейде."
-L["Arena"] = "Арена"
-L["Show while in a PvP arena"] = "Показывать, когда на арене(PvP)."
-L["Battleground"] = "Поле боя"
-L["Show while in a PvP battleground"] = "Показывать, когда на поле боя(PvP)."
+	["Overwritten"] = "Перебит",
+	["Set the color for the target name when your %s has been overwritten."] = "Использовать этот цвет для цели, на которой %s, в случае если чужой щит перебил ваш на этой цели.",
 
--- L["Exceptions"] = ""
--- L["Dead"] = ""
--- L["Hide the display while you are dead"] = ""
--- L["Out Of Combat"] = ""
--- L["Hide the display while you are out of combat"] = ""
--- L["Resting"] = ""
--- L["Hide the display while you are in an inn or major city"] = ""
--- L["Vehicle"] = ""
--- L["Hide the display while you are controlling a vehicle"] = ""
+	["Inactive"] = "Предупреждение",
+	["Set the color for expired, dispelled, or otherwise inactive shields."] = "Использовать этот цвет, когда счечик зарядов на нуле.",
+
+	["Colorblind Mode"] = USE_COLORBLIND_MODE, -- Leave this as-is unless there is something wrong with Blizzard's translation in your locale
+--	["Add asterisks around the target name when your %s has been overwritten, in addition to changing the color."] = "",
 
 ------------------------------------------------------------------------
+
+	["Alerts"] = "Предупреждения",
+--	["Use these settings to configure how ShieldsUp alerts you when a shield expires or is removed."] = "",
+
+	["Text Alert"] = "Текст",
+	["Show a text message when %s expires."] = "Показывать предупреждение, когда спадает %s.",
+
+	["Sound Alert"] = "Звук",
+	["Play a sound when %s expires."] = "Проигрывать звук, когда спадает %s.",
+
+	["Sound File"] = "Звуковой файл",
+--	["Select the sound to play when %s expires."] = "",
+
+--	["Text Output"] = "",
+
+------------------------------------------------------------------------
+
+	["Visibility"] = "Видимость",
+	["Use these settings to control when the ShieldsUp display should be shown or hidden."] = "Настройка отображения в зависимости от следующих условий.",
+--	["Enable"] = "",
+
+	["Group Size"] = "Размер группы",
+	["Solo"] = "Соло",
+	["Show the display while you are not in a group"] = "Показывать, когда не в группе.",
+	["Party"] = "Группа",
+	["Show the display while you are in a 5-man party"] = "Показывать, когда в группе из 5 человек.",
+	["Raid"] = "Рейд",
+	["Show the display while you are in a raid group"] = "Показывать, когда в рейд.е",
+
+	["Zone Type"] = "Тип зоны",
+	["World"] = "Мир",
+	["Show the display while you are in the outdoor world"] = "Показывать, когда в мире.",
+	["Dungeon"] = "Подземелье",
+	["Show the display while you are in a party dungeon"] = "Показывать, когда в подземелье на 5 человек.",
+	["Raid Dungeon"] = "Инстанс-рейд",
+	["Show the display while you are in a raid dungeon"] = "Показывать, когда в инстансе-рейде.",
+	["Arena"] = "Арена",
+	["Show the display while you are in a PvP arena"] = "Показывать, когда на арене(PvP).",
+	["Battleground"] = "Поле боя",
+	["Show the display while you are in a PvP battleground"] = "Показывать, когда на поле боя(PvP).",
+
+--	["Exceptions"] = "",
+--	["Dead"] = "",
+--	["Hide the display while you are dead"] = "",
+--	["Out Of Combat"] = "",
+--	["Hide the display while you are out of combat"] = "",
+--	["Resting"] = "",
+--	["Hide the display while you are in an inn or major city"] = "",
+--	["Vehicle"] = "",
+--	["Hide the display while you are controlling a vehicle"] = "",
+
+------------------------------------------------------------------------
+
+}
--- Locales/Locale-zhCN.lua
+++ Locales/Locale-zhCN.lua
@@ -13,129 +13,130 @@
 
 if GetLocale() ~= "zhCN" or select(2, UnitClass("player")) ~= "SHAMAN" then return end
 
-local L = { }
-local ADDON_NAME, namespace = ...
-if not namespace then namespace = { } _G.ShieldsUpNamespace = namespace end namespace.L = L
+local _, namespace = ...; if not namespace then namespace = { } _G.ShieldsUpNamespace = namespace end
+namespace.L = {
 
 ------------------------------------------------------------------------
 -- These strings are displayed when shields expire.
 
-L["%s faded!"] = "%s 已消失"
-L["%s faded from %s!"] = "%s 在 %s 上消失!"
-L["YOU"] = "自身"
+	["%s faded!"] = "%s 已消失",
+	["%s faded from %s!"] = "%s 在 %s 上消失!",
+	["YOU"] = "自身",
 
 ------------------------------------------------------------------------
 -- These strings are displayed in the configuration GUI.
 
--- L["Click for options."] = ""
+--	["Click for options."] = "",
 
-L["ShieldsUp is a monitor for your shaman shields. Use these settings to configure the addon's appearance and behavior."] = "ShieldsUp是一个萨满护盾的监视器。使用这些设定来配置插件的外观和行为."
+	["ShieldsUp is a monitor for your shaman shields. Use these settings to configure the addon's appearance and behavior."] = "ShieldsUp是一个萨满护盾的监视器。使用这些设定来配置插件的外观和行为.",
 
-L["Horizontal Position"] = "水平位置"
-L["Set the horizontal distance from the center of the screen to place the display."] = "设定插件显示的相对于屏幕中间的水平位置."
+	["Horizontal Position"] = "水平位置",
+	["Set the horizontal distance from the center of the screen to place the display."] = "设定插件显示的相对于屏幕中间的水平位置.",
 
-L["Vertical Position"] = "垂直位置"
-L["Set the vertical distance from the center of the screen to place the display."] = "设定插件显示的相对于屏幕中间的水平位置."
+	["Vertical Position"] = "垂直位置",
+	["Set the vertical distance from the center of the screen to place the display."] = "设定插件显示的相对于屏幕中间的水平位置.",
 
-L["Horizontal Padding"] = "水平间距"
-L["Set the horizontal space between the charge counts."] = "设定计数器的水平距离."
+	["Horizontal Padding"] = "水平间距",
+	["Set the horizontal space between the charge counters."] = "设定计数器的水平距离.",
 
-L["Vertical Padding"] = "垂直间距"
-L["Set the vertical space between the target name and charge counters."] = "设定目标名字和计数器的垂直距离."
+	["Vertical Padding"] = "垂直间距",
+	["Set the vertical space between the target name and charge counters."] = "设定目标名字和计数器的垂直距离.",
 
-L["Opacity"] = "不透明度"
-L["Set the opacity level for the display."] = "设定显示的不透明度等级."
+	["Opacity"] = "不透明度",
+	["Set the opacity level for the display."] = "设定显示的不透明度等级.",
 
--- L["Overwrite Alert"] = ""
--- L["Print a message in the chat frame alerting you who overwrites your %s."] = ""
-
-------------------------------------------------------------------------
-
-L["Font Face"] = "字体"
-L["Set the font face to use for the display text."] = "设定显示文字的字体."
-
-L["Outline"] = "描边"
-L["Select an outline width for the display text."] = "设定显示文字的描边宽度"
-L["None"] = "无"
-L["Thin"] = "细描边"
-L["Thick"] = "粗描边"
-
-L["Shadow"] = "阴影效果"
-L["Add a drop shadow effect to the display text."] = "为显示文字增加一个阴影效果."
-
-L["Counter Size"] = "计数器大小"
-L["Set the text size for the charge counters."] = "设定计数器的文字大小."
-
-L["Name Size"] = "名字大小"
-L["Set the text size for the target name."] = "设定目标名字的文字大小."
+--	["Overwrite Alert"] = "",
+--	["Print a message in the chat frame alerting you who overwrites your %s."] = "",
 
 ------------------------------------------------------------------------
 
-L["Colors"] = "颜色"
-L["Set the color for the %s charge counter."] = "设定 %s 计数器的颜色."
+	["Font Face"] = "字体",
+	["Set the font face to use for the display text."] = "设定显示文字的字体.",
 
-L["Active"] = "激活"
-L["Set the color for the target name while your %s is active."] = "设定当你的 %s 已激活时目标名字的颜色."
+	["Outline"] = "描边",
+	["Select an outline width for the display text."] = "设定显示文字的描边宽度",
+	["None"] = "无",
+	["Thin"] = "细描边",
+	["Thick"] = "粗描边",
 
-L["Overwritten"] = "覆盖"
-L["Set the color for the target name when your %s has been overwritten."] = "设定当你的 %s 被其他人覆盖时目标名字的颜色"
+	["Shadow"] = "阴影效果",
+	["Add a drop shadow effect to the display text."] = "为显示文字增加一个阴影效果.",
 
-L["Inactive"] = "未激活"
-L["Set the color for expired, dispelled, or otherwise inactive shields."] = "设定当你的护盾过期，被驱散或者其他未被激活的情况下的颜色."
+	["Counter Size"] = "计数器大小",
+	["Set the text size for the charge counters."] = "设定计数器的文字大小.",
 
-L["Colorblind Mode"] = USE_COLORBLIND_MODE -- Leave this as-is unless there is something wrong with Blizzard's translation in your locale
-L["Add asterisks around the target name when your %s has been overwritten, in addition to changing the color."] = "当你的 %s 被其他人覆盖时除了改变颜色外, 还为目标名字增加 * 号."
-
-------------------------------------------------------------------------
-
-L["Alerts"] = "警报"
-L["Use these settings to configure how ShieldsUp alerts you when a shield expires or is removed."] = "使用这些警报来配置当一个护盾过期或者被移除时发出 ShieldsUp 警报."
-
-L["Text Alert"] = "文字警报"
-L["Show a text message when %s expires."] = "当 %s 到期时显示一个文本讯息."
-
-L["Sound Alert"] = "音效警报"
-L["Play a sound when %s expires."] = "当 %s 到期时播放一个音效警报."
-
-L["Sound File"] = "音效文件"
-L["Select the sound to play when %s expires."] = "选择当 %s 到期时音效警报的文件"
-
-L["Text Output"] = "文字输出"
+	["Name Size"] = "名字大小",
+	["Set the text size for the target name."] = "设定目标名字的文字大小.",
 
 ------------------------------------------------------------------------
 
-L["Visibility"] = "能见度"
-L["Enable"] = "开启"
-L["Use these settings to control when the ShieldsUp display should be shown or hidden."] = "使用这些设定来控制 ShieldsUp 什么时候显示或隐藏."
+	["Colors"] = "颜色",
+	["Set the color for the %s charge counter."] = "设定 %s 计数器的颜色.",
 
-L["Group Size"] = "队伍大小"
-L["Solo"] = "独自战斗"
-L["Show the display while you are not in a group"] = "当你不在一个队伍时显示"
-L["Party"] = "小队"
-L["Show the display while you are in a party group"] = "当你在一个小队时显示"
-L["Raid"] = "团队"
-L["Show the display while you are in a raid group"] = "当你在团队时显示"
+	["Active"] = "激活",
+	["Set the color for the target name while your %s is active."] = "设定当你的 %s 已激活时目标名字的颜色.",
 
-L["Zone Type"] = "区域类型"
-L["World"] = "户外"
-L["Show the display while you are in the outdoor world"] = "当你在户外世界时显示."
-L["Party Dungeon"] = "小队副本"
-L["Show the display while you are in a party dungeon"] = "当你在小队副本时显示."
-L["Raid Dungeon"] = "团队副本"
-L["Show the display while you are in a raid dungeon"] = "当你在团队副本时显示"
-L["Arena"] = "竞技场"
-L["Show the display while you are in a PvP arena"] = "当你在一个PvP竞技场时显示"
-L["Battleground"] = "战场"
-L["Show the display while you are in a PvP battleground"] = "当你在一个PvP战场时显示."
+	["Overwritten"] = "覆盖",
+	["Set the color for the target name when your %s has been overwritten."] = "设定当你的 %s 被其他人覆盖时目标名字的颜色",
 
-L["Exceptions"] = "排除"
-L["Dead"] = "死亡"
-L["Hide the display while you are dead"] = "当你死亡时隐藏"
-L["Out Of Combat"] = "脱离战斗状态"
-L["Hide the display while you are out of combat"] = "当你脱离战斗状态时隐藏"
-L["Resting"] = "休息状态"
-L["Hide the display while you are in an inn or major city"] = "当你在旅馆或者在主城时隐藏"
-L["Vehicle"] = "载具"
-L["Hide the display while you are controlling a vehicle"] = "当你正在控制一个载具时隐藏"
+	["Inactive"] = "未激活",
+	["Set the color for expired, dispelled, or otherwise inactive shields."] = "设定当你的护盾过期，被驱散或者其他未被激活的情况下的颜色.",
+
+	["Colorblind Mode"] = USE_COLORBLIND_MODE, -- Leave this as-is unless there is something wrong with Blizzard's translation in your locale
+	["Add asterisks around the target name when your %s has been overwritten, in addition to changing the color."] = "当你的 %s 被其他人覆盖时除了改变颜色外, 还为目标名字增加 * 号.",
 
 ------------------------------------------------------------------------
+
+	["Alerts"] = "警报",
+	["Use these settings to configure how ShieldsUp alerts you when a shield expires or is removed."] = "使用这些警报来配置当一个护盾过期或者被移除时发出 ShieldsUp 警报.",
+
+	["Text Alert"] = "文字警报",
+	["Show a text message when %s expires."] = "当 %s 到期时显示一个文本讯息.",
+
+	["Sound Alert"] = "音效警报",
+	["Play a sound when %s expires."] = "当 %s 到期时播放一个音效警报.",
+
+	["Sound File"] = "音效文件",
+	["Select the sound to play when %s expires."] = "选择当 %s 到期时音效警报的文件",
+
+	["Text Output"] = "文字输出",
+
+------------------------------------------------------------------------
+
+	["Visibility"] = "能见度",
+	["Use these settings to control when ShieldsUp should be shown or hidden."] = "使用这些设定来控制 ShieldsUp 什么时候显示或隐藏.",
+	["Enable"] = "开启",
+
+	["Group Size"] = "队伍大小",
+	["Solo"] = "独自战斗",
+	["Show the display while you are not in a group"] = "当你不在一个队伍时显示",
+	["Party"] = "小队",
+	["Show the display while you are in a party group"] = "当你在一个小队时显示",
+	["Raid"] = "团队",
+	["Show the display while you are in a raid group"] = "当你在团队时显示",
+
+	["Zone Type"] = "区域类型",
+	["World"] = "户外",
+	["Show the display while you are in the outdoor world"] = "当你在户外世界时显示.",
+	["Dungeon"] = "小队副本",
+	["Show the display while you are in a party dungeon"] = "当你在小队副本时显示.",
+	["Raid Dungeon"] = "团队副本",
+	["Show the display while you are in a raid dungeon"] = "当你在团队副本时显示",
+	["Arena"] = "竞技场",
+	["Show the display while you are in a PvP arena"] = "当你在一个PvP竞技场时显示",
+	["Battleground"] = "战场",
+	["Show the display while you are in a PvP battleground"] = "当你在一个PvP战场时显示.",
+
+	["Exceptions"] = "排除",
+	["Dead"] = "死亡",
+	["Hide the display while you are dead"] = "当你死亡时隐藏",
+	["Out Of Combat"] = "脱离战斗状态",
+	["Hide the display while you are out of combat"] = "当你脱离战斗状态时隐藏",
+	["Resting"] = "休息状态",
+	["Hide the display while you are in an inn or major city"] = "当你在旅馆或者在主城时隐藏",
+	["Vehicle"] = "载具",
+	["Hide the display while you are controlling a vehicle"] = "当你正在控制一个载具时隐藏",
+
+------------------------------------------------------------------------
+
+}
