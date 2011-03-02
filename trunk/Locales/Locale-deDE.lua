99eac1d7291b5593491653d544b3fff237eadfe1
Akkorian-257529 <Akkorian-257529@0b1653cb-a12f-4a2b-9757-4012de170f90>
2011-03-02 04:05:31 +0000

    Updated localization files

--- trunk/Locales/Locale-deDE.lua
+++ trunk/Locales/Locale-deDE.lua
@@ -1,4 +1,4 @@
-﻿--[[--------------------------------------------------------------------
+﻿
 	ShieldsUp
 	Text-based shaman shield monitor.
 	Written by Phanx <addons@phanx.net>
@@ -13,129 +13,130 @@
 
 if GetLocale() ~= "deDE" or select(2, UnitClass("player")) ~= "SHAMAN" then return end
 
-local L = { }
-local ADDON_NAME, namespace = ...
-namespace.L = L
+local _, namespace = ...
+namespace.L = {
 
 ------------------------------------------------------------------------
 -- These strings are displayed when shields expire.
 
-L["%s faded!"] = "%s aufgebraucht!"
-L["%s faded from %s!"] = "%s von %s aufgebraucht!"
-L["YOU"] = "DIR"
+	["%s faded!"] = "%s aufgebraucht!",
+	["%s faded from %s!"] = "%s von %s aufgebraucht!",
+	["YOU"] = "DIR",
 
 ------------------------------------------------------------------------
 -- These strings are displayed in the configuration GUI.
 
-L["Click for options."] = "Linksklick für Optionen."
+	["Click for options."] = "Linksklick für Optionen.",
 
-L["ShieldsUp is a simple monitor for your shaman shields. Use these settings to configure the addon's appearance and behavior."] = "ShieldsUp is eine einfache Anzeige für die verschiedenen Schilde des Schamanen. Benutze diese Optionen um das Erscheinungsbild und Verhalten zu konfigurieren."
+	["ShieldsUp is a simple monitor for your shaman shields. Use these settings to configure the addon's appearance and behavior."] = "ShieldsUp is eine einfache Anzeige für die verschiedenen Schilde des Schamanen. Benutze diese Optionen um das Erscheinungsbild und Verhalten zu konfigurieren.",
 
-L["Horizontal Position"] = "Horizontale Position"
-L["Set the horizontal distance from the center of the screen to place the display."] = "Einstellen des horizontalen Abstandes von der Mitte(des Bildschirms) zum gewünschten Platz von ShieldsUp."
+	["Horizontal Position"] = "Horizontale Position",
+	["Set the horizontal distance from the center of the screen to place the display."] = "Einstellen des horizontalen Abstandes von der Mitte(des Bildschirms) zum gewünschten Platz von ShieldsUp.",
 
-L["Vertical Position"] = "Vertikale Position"
-L["Set the vertical distance from the center of the screen to place the display."] = "Einstellen des vertikalen Abstandes von der Mitte(des Bildschirms) zum gewünschten Platz von ShieldsUp."
+	["Vertical Position"] = "Vertikale Position",
+	["Set the vertical distance from the center of the screen to place the display."] = "Einstellen des vertikalen Abstandes von der Mitte(des Bildschirms) zum gewünschten Platz von ShieldsUp.",
 
-L["Horizontal Padding"] = "Horizontale Abstimmung"
-L["Set the horizontal space between the charge counts."] = "Wähle den Abstand zwischen den Aufladungen der Schilde."
+	["Horizontal Padding"] = "Horizontale Abstimmung",
+	["Set the horizontal space between the charge counters."] = "Wähle den Abstand zwischen den Aufladungen der Schilde.",
 
-L["Vertical Padding"] = "Vertikale Abstimmung"
-L["Set the vertical space between the target name and charge counters."] = "Wähle den Abstand zwischen dem Zielnamen und den Aufladungen."
+	["Vertical Padding"] = "Vertikale Abstimmung",
+	["Set the vertical space between the target name and charge counters."] = "Wähle den Abstand zwischen dem Zielnamen und den Aufladungen.",
 
-L["Opacity"] = "Durchsichtigkeit"
-L["Set the opacity level for the display."] = "Konfiguriere die Durchsichtigkeit der Anzeige."
+	["Opacity"] = "Durchsichtigkeit",
+	["Set the opacity level for the display."] = "Konfiguriere die Durchsichtigkeit der Anzeige.",
 
--- L["Overwrite Alert"] = ""
--- L["Print a message in the chat frame alerting you who overwrites your %s."] = ""
-
-------------------------------------------------------------------------
-
-L["Font Face"] = "Textfont"
-L["Set the font face to use for the display text."] = "Wähle den Font für den angezeigten Text."
-
-L["Outline"] = "Umrandung"
-L["Select an outline width for the display text."] = "Wähle eine Umrandung für den angezeigten Text"
-L["None"] = "Keine"
-L["Thin"] = "Dünn"
-L["Thick"] = "Dick"
-
-L["Shadow"] = "Schatten"
-L["Add a drop shadow effect to the display text."] = "Füge einen Schatteneffekt hinzu."
-
-L["Counter Size"] = "Zahlengröße"
-L["Set the text size for the charge counters."] = "Wähle die Textgröße für die Zahlen."
-
-L["Name Size"] = "Namensgröße"
-L["Set the text size for the target name."] = "Wähle die Textgröße des Zielnamens."
+--	["Overwrite Alert"] = "",
+--	["Print a message in the chat frame alerting you who overwrites your %s."] = "",
 
 ------------------------------------------------------------------------
 
-L["Colors"] = "Farben"
-L["Set the color for the %s charge counter."] = "Wähle die Textgröße der Aufladungen von %s."
+	["Font Face"] = "Textfont",
+	["Set the font face to use for the display text."] = "Wähle den Font für den angezeigten Text.",
 
-L["Active"] = "Aktiv"
-L["Set the color for the target name while your %s is active."] = "Wähle die Farbe für den Namen deines Ziels während %s ist aktiv."
+	["Outline"] = "Umrandung",
+	["Select an outline width for the display text."] = "Wähle eine Umrandung für den angezeigten Text",
+	["None"] = "Keine",
+	["Thin"] = "Dünn",
+	["Thick"] = "Dick",
 
-L["Overwritten"] = "Überschrieben"
-L["Set the color for the target name when your %s has been overwritten."] = "Wähle die Farbe für den Zielnamen, falls dein %s von einem anderen Schamanen überzaubert wurde"
+	["Shadow"] = "Schatten",
+	["Add a drop shadow effect to the display text."] = "Füge einen Schatteneffekt hinzu.",
 
-L["Inactive"] = "Inaktiv"
-L["Set the color for expired, dispelled, or otherwise inactive shields."] = "Wähe die Farbe für abgelaufene, entzauberte oder inaktive Schilde."
+	["Counter Size"] = "Zahlengröße",
+	["Set the text size for the charge counters."] = "Wähle die Textgröße für die Zahlen.",
 
-L["Colorblind Mode"] = USE_COLORBLIND_MODE -- Leave this as-is unless there is something wrong with Blizzard's translation in your locale
-L["Add asterisks around the target name when your %s has been overwritten, in addition to changing the color."] = "Fügt * um den Zielnamen, falls dein %s auf dem Ziel überzaubert wurde, zusätzlich zum Farbenwechsel."
-
-------------------------------------------------------------------------
-
-L["Alerts"] = "Warnungen"
-L["Use these settings to configure how ShieldsUp alerts you when a shield expires or is removed."] = "Verwende diese Optionen um ShieldsUp zu konfigurieren wie es dich bei abgelaufenen oder entzauberten Schilden informieren soll."
-
-L["Text Alert"] = "Textwarnungen"
-L["Show a text message when %s expires."] = "Zeigt eine Nachricht, wenn %s abläuft."
-
-L["Sound Alert"] = "Akustische Warnungen"
-L["Play a sound when %s expires."] = "Spielt einen Ton ab, wenn %s abläuft."
-
-L["Sound File"] = "Musikdatei"
-L["Select the sound to play when %s expires."] = "Wähle eine Musikdatei zum abspielen, wenn %s abläuft"
-
-L["Text Output"] = "Textausgabe"
+	["Name Size"] = "Namensgröße",
+	["Set the text size for the target name."] = "Wähle die Textgröße des Zielnamens.",
 
 ------------------------------------------------------------------------
 
-L["Visibility"] = "Sichtbarkeit"
-L["Enable"] = "Aktivieren"
-L["Use these settings to control when ShieldsUp display should be shown or hidden."] = "Ändere diese Einstellungen um ShieldsUp unter bestimmten Voraussetzungen zu zeigen oder zu verstecken."
+	["Colors"] = "Farben",
+	["Set the color for the %s charge counter."] = "Wähle die Textgröße der Aufladungen von %s.",
 
-L["Group Size"] = "Gruppengröße"
--- L["Solo"] = ""
-L["Show the display while you are not in a group"] = "Zeige ShieldsUp außerhalb einer Gruppe."
--- L["Party"] = ""
-L["Show the display while you are in a party group"] = "Zeige ShieldsUp in einer 5er Gruppe."
-L["Raid"] = "Schlachtzug"
-L["Show the display while you are in a raid group"] = "Zeige ShieldsUp wenn man sich in einem Schlachtzug befindet"
+	["Active"] = "Aktiv",
+	["Set the color for the target name while your %s is active."] = "Wähle die Farbe für den Namen deines Ziels während %s ist aktiv.",
 
-L["Zone Type"] = "Art der Zone"
-L["World"] = "Welt"
-L["Show the display while you are in the outdoor world"] = "Zeige ShieldsUp außerhalb von allen Spezialgebiten (Instanz/Schlachtzug...) ."
-L["Dungeon"] = "Instanz"
-L["Show the display while you are in a party dungeon"] = "Zeige ShieldsUp während du dich in einer 5er Instanz befindest."
-L["Raid Dungeon"] = "Schlachtzugsinstanz"
-L["Show the display while you are in a raid dungeon"] = "Zeige ShieldsUp in einer Schlachtzugsinstanz "
--- L["Arena"] = ""
-L["Show the display while you are in a PvP arena"] = "Zeige ShieldsUp während du dich in einer Arena befindest."
-L["Battleground"] = "Schlachtfeld"
-L["Show the display while you are in a PvP battleground"] = "Zeige ShieldsUp während du dich auf einem Schlachtfeld befindest."
+	["Overwritten"] = "Überschrieben",
+	["Set the color for the target name when your %s has been overwritten."] = "Wähle die Farbe für den Zielnamen, falls dein %s von einem anderen Schamanen überzaubert wurde",
 
-L["Exceptions"] = "Außnahmen"
-L["Dead"] = "Tot"
-L["Hide the display while you are dead"] = "Verstecke ShieldsUp wenn du tot bist"
-L["Out Of Combat"] = "Außerhalb eines Kampfes"
-L["Hide the display while you are out of combat"] = "Verstecke ShieldsUp außerhalb des Kampfes"
-L["Resting"] = "Erholt"
-L["Hide the display while you are in an inn or major city"] = "Verstecke ShieldsUp während du dich bei einem Gastwirt oder in einer Hauptstadt aufhälst"
-L["Vehicle"] = "Fahrzeug"
-L["Hide the display while you are controlling a vehicle"] = "Verstecke ShieldsUp während du ein Fahrzeug steuerst"
+	["Inactive"] = "Inaktiv",
+	["Set the color for expired, dispelled, or otherwise inactive shields."] = "Wähe die Farbe für abgelaufene, entzauberte oder inaktive Schilde.",
+
+	["Colorblind Mode"] = USE_COLORBLIND_MODE, -- Leave this as-is unless there is something wrong with Blizzard's translation in your locale
+	["Add asterisks around the target name when your %s has been overwritten, in addition to changing the color."] = "Fügt * um den Zielnamen, falls dein %s auf dem Ziel überzaubert wurde, zusätzlich zum Farbenwechsel.",
 
 ------------------------------------------------------------------------
+
+	["Alerts"] = "Warnungen",
+	["Use these settings to configure how ShieldsUp alerts you when a shield expires or is removed."] = "Verwende diese Optionen um ShieldsUp zu konfigurieren wie es dich bei abgelaufenen oder entzauberten Schilden informieren soll.",
+
+	["Text Alert"] = "Textwarnungen",
+	["Show a text message when %s expires."] = "Zeigt eine Nachricht, wenn %s abläuft.",
+
+	["Sound Alert"] = "Akustische Warnungen",
+	["Play a sound when %s expires."] = "Spielt einen Ton ab, wenn %s abläuft.",
+
+	["Sound File"] = "Musikdatei",
+	["Select the sound to play when %s expires."] = "Wähle eine Musikdatei zum abspielen, wenn %s abläuft",
+
+	["Text Output"] = "Textausgabe",
+
+------------------------------------------------------------------------
+
+	["Visibility"] = "Sichtbarkeit",
+	["Use these settings to control when ShieldsUp should be shown or hidden."] = "Ändere diese Einstellungen um ShieldsUp unter bestimmten Voraussetzungen zu zeigen oder zu verstecken.",
+	["Enable"] = "Aktivieren",
+
+	["Group Size"] = "Gruppengröße",
+--	["Solo"] = "",
+	["Show the display while you are not in a group"] = "Zeige ShieldsUp außerhalb einer Gruppe.",
+--	["Party"] = "",
+	["Show the display while you are in a party group"] = "Zeige ShieldsUp in einer 5er Gruppe.",
+	["Raid"] = "Schlachtzug",
+	["Show the display while you are in a raid group"] = "Zeige ShieldsUp wenn man sich in einem Schlachtzug befindet",
+
+	["Zone Type"] = "Art der Zone",
+	["World"] = "Welt",
+	["Show the display while you are in the outdoor world"] = "Zeige ShieldsUp außerhalb von allen Spezialgebiten (Instanz/Schlachtzug...) .",
+	["Dungeon"] = "Instanz",
+	["Show the display while you are in a party dungeon"] = "Zeige ShieldsUp während du dich in einer 5er Instanz befindest.",
+	["Raid Dungeon"] = "Schlachtzugsinstanz",
+	["Show the display while you are in a raid dungeon"] = "Zeige ShieldsUp in einer Schlachtzugsinstanz ",
+--	["Arena"] = "",
+	["Show the display while you are in a PvP arena"] = "Zeige ShieldsUp während du dich in einer Arena befindest.",
+	["Battleground"] = "Schlachtfeld",
+	["Show the display while you are in a PvP battleground"] = "Zeige ShieldsUp während du dich auf einem Schlachtfeld befindest.",
+
+	["Exceptions"] = "Außnahmen",
+	["Dead"] = "Tot",
+	["Hide the display while you are dead"] = "Verstecke ShieldsUp wenn du tot bist",
+	["Out Of Combat"] = "Außerhalb eines Kampfes",
+	["Hide the display while you are out of combat"] = "Verstecke ShieldsUp außerhalb des Kampfes",
+	["Resting"] = "Erholt",
+	["Hide the display while you are in an inn or major city"] = "Verstecke ShieldsUp während du dich bei einem Gastwirt oder in einer Hauptstadt aufhälst",
+	["Vehicle"] = "Fahrzeug",
+	["Hide the display while you are controlling a vehicle"] = "Verstecke ShieldsUp während du ein Fahrzeug steuerst",
+
+------------------------------------------------------------------------
+
+}
--- trunk/Locales/Locale-esES.lua
+++ trunk/Locales/Locale-esES.lua
@@ -13,129 +13,130 @@
 
 if not (GetLocale() == "esES" or GetLocale() == "esMX") or select(2, UnitClass("player")) ~= "SHAMAN" then return end
 
-local L = { }
-local ADDON_NAME, namespace = ...
-namespace.L = L
+local _, namespace = ...
+namespace.L = {
 
 ------------------------------------------------------------------------
 -- These strings are displayed when shields expire.
 
-L["%s faded!"] = "%s desapareció!"
-L["%s faded from %s!"] = "%s desapareció de %s!"
-L["YOU"] = "USTED"
+	["%s faded!"] = "%s desapareció!",
+	["%s faded from %s!"] = "%s desapareció de %s!",
+	["YOU"] = "TÚ MISMO",
 
 ------------------------------------------------------------------------
 -- These strings are displayed in the configuration GUI.
 
-L["Click for options."] = "Haz clic para abrir las opciones."
+	["Click for options."] = "Haz clic para abrir las opciones.",
 
-L["ShieldsUp is a simple monitor for your shaman shields. Use these settings to configure the addon's appearance and behavior."] = "Estas opciones te permiten modificar la configuración de ShieldsUp, un addon para seguir tus Escudos elementales."
+	["ShieldsUp is a simple monitor for your shaman shields. Use these settings to configure the addon's appearance and behavior."] = "Estas opciones te permiten modificar la configuración de ShieldsUp, un addon para seguir tus Escudos elementales.",
 
-L["Horizontal Position"] = "Posición horizontal"
-L["Set the horizontal distance from the center of the screen to place the display."] = "Ajustar la distancia horizontal desde el centro de la pantalla para colocar el marco."
+	["Horizontal Position"] = "Posición horizontal",
+	["Set the horizontal distance from the center of the screen to place the display."] = "Ajustar la distancia horizontal desde el centro de la pantalla para colocar el marco.",
 
-L["Vertical Position"] = "Posición vertical"
-L["Set the vertical distance from the center of the screen to place the display."] = "Ajustar la distancia vertical desde el centro de la pantalla para colocar el marco."
+	["Vertical Position"] = "Posición vertical",
+	["Set the vertical distance from the center of the screen to place the display."] = "Ajustar la distancia vertical desde el centro de la pantalla para colocar el marco.",
 
-L["Horizontal Padding"] = "Espaciado horizontal"
-L["Set the horizontal space between the charge counts."] = "Ajustar el espacio horizontal entre los números de cargas."
+	["Horizontal Padding"] = "Espaciado horizontal",
+	["Set the horizontal space between the charge counters."] = "Ajustar el espacio horizontal entre los números de cargas.",
 
-L["Vertical Padding"] = "Espaciado vertical"
-L["Set the vertical space between the target name and charge counters."] = "Ajustar el espacio vertical entre el nombre objetivo y los números de cargas."
+	["Vertical Padding"] = "Espaciado vertical",
+	["Set the vertical space between the target name and charge counters."] = "Ajustar el espacio vertical entre el nombre objetivo y los números de cargas.",
 
-L["Opacity"] = "Opacidad"
-L["Set the opacity level for the display."] = "Ajustar la opacidad del marco."
+	["Opacity"] = "Opacidad",
+	["Set the opacity level for the display."] = "Ajustar la opacidad del marco.",
 
-L["Overwrite Alert"] = "Aviso en sobrescrito"
-L["Print a message in the chat frame alerting you who overwrites your %s."] = "Mostrar una aviso en la ventana de chat cuando otro chamán sobrescribe tu %s."
-
-------------------------------------------------------------------------
-
-L["Font Face"] = "Fuente"
-L["Set the font face to use for the display text."] = "Cambiar la fuente."
-
-L["Outline"] = "Perfil de fuente"
-L["Select an outline width for the display text."] = "Ajustar el perfil de la fuente."
-L["None"] = "Ninguno"
-L["Thin"] = "Fino"
-L["Thick"] = "Grueso"
-
-L["Shadow"] = "Sombra de fuente"
-L["Add a drop shadow effect to the display text."] = "Mostrar la sombra de la fuente."
-
-L["Counter Size"] = "Tamaño de números"
-L["Set the text size for the charge counters."] = "Ajustar el tamaño de los números de cargas."
-
-L["Name Size"] = "Tamaño de nombre"
-L["Set the text size for the target name."] = "Ajustar el tamaño de la fuente del nombre objetivo."
+	["Overwrite Alert"] = "Aviso en sobrescrito",
+	["Print a message in the chat frame alerting you who overwrites your %s."] = "Mostrar una aviso en la ventana de chat cuando otro chamán sobrescribe tu %s.",
 
 ------------------------------------------------------------------------
 
-L["Colors"] = "Colores"
-L["Set the color for the %s charge counter."] = "Establecer el color del número de cargas en tu %s."
+	["Font Face"] = "Fuente",
+	["Set the font face to use for the display text."] = "Cambiar la fuente.",
 
-L["Active"] = "Activo"
-L["Set the color for the target name while your %s is active."] = "Establecer el color del nombre objetivo, mientras que su %s está activo."
+	["Outline"] = "Perfil de fuente",
+	["Select an outline width for the display text."] = "Ajustar el perfil de la fuente.",
+	["None"] = "Ninguno",
+	["Thin"] = "Fino",
+	["Thick"] = "Grueso",
 
-L["Overwritten"] = "Sobrescrito"
-L["Set the color for the target name when your %s has been overwritten."] = "Ajusta el color del nombre objetivo, mientras que tu %s se ha sobrescrito."
+	["Shadow"] = "Sombra de fuente",
+	["Add a drop shadow effect to the display text."] = "Mostrar la sombra de la fuente.",
 
-L["Inactive"] = "Inactivo"
-L["Set the color for expired, dispelled, or otherwise inactive shields."] = "Ajusta el color de los números de cargas en Escudos inactivos."
+	["Counter Size"] = "Tamaño de números",
+	["Set the text size for the charge counters."] = "Ajustar el tamaño de los números de cargas.",
 
-L["Colorblind Mode"] = USE_COLORBLIND_MODE -- Leave this as-is unless there is something wrong with Blizzard's translation in your locale
-L["Add asterisks around the target name when your %s has been overwritten, in addition to changing the color."] = "Añadir asteriscos alrededor del nombre de objetivo cuando otro chamán sobrescribe tu %s, además de cambiar el color."
-
-------------------------------------------------------------------------
-
-L["Alerts"] = "Alertas"
-L["Use these settings to configure how ShieldsUp alerts you when a shield expires or is removed."] = "Estas opciones te permiten cambiar la forma en ShieldsUp te avisa en un Escudo expira o se quita."
-
-L["Text Alert"] = "Alertas de texto"
-L["Show a text message when %s expires."] = "Mostrar un mensaje de alerta cuando expira tu %s."
-
-L["Sound Alert"] = "Alertas de sonido"
-L["Play a sound when %s expires."] = "Reproducir un sonido cuando expira tu %s."
-
-L["Sound File"] = "Sonido"
-L["Select the sound to play when %s expires."] = "Establecer el sonido a reproducir cuando expira su %s."
-
-L["Text Output"] = "Salida de alertas de texto"
+	["Name Size"] = "Tamaño de nombre",
+	["Set the text size for the target name."] = "Ajustar el tamaño de la fuente del nombre objetivo.",
 
 ------------------------------------------------------------------------
 
-L["Visibility"] = "Visibilidad"
-L["Use these settings to control when ShieldsUp display should be shown or hidden."] = "Estas opciones le permiten controlar cuando el marco de ShieldsUp se muestran o se ocultan."
-L["Enable"] = "Activar"
+	["Colors"] = "Colores",
+	["Set the color for the %s charge counter."] = "Ajustar el color del número de cargas en tu %s.",
 
-L["Group Size"] = "Tamaño de grupo"
--- L["Solo"] = ""
-L["Show the display while you are not in a group"] = "Mostrar el marco mientras estás solo."
-L["Party"] = "Grupo"
-L["Show the display while you are in a party group"] = "Mostrar el marco mientras estás en un grupo de 5."
-L["Raid"] = "Banda"
-L["Show the display while you are in a raid group"] = "Mostrar el marco mientras estás en un banda."
+	["Active"] = "Activo",
+	["Set the color for the target name while your %s is active."] = "Ajustar el color del nombre objetivo, mientras que tu %s está activo.",
 
-L["Zone Type"] = "Tipo de Zona"
-L["World"] = "Mundo"
-L["Show the display while you are in the outdoor world"] = "Mostrar el marco mientras estás en el mundo, no una mazmorra."
-L["Dungeon"] = "Mazmorra"
-L["Show the display while you are in a party dungeon"] = "Mostrar el marco mientras estás en una mazmorra."
-L["Raid Dungeon"] = "Mazmorra de banda"
-L["Show the display while you are in a raid dungeon"] = "Mostrar el marco mientras estás en una mazmorra de banda."
--- L["Arena"] = ""
-L["Show the display while you are in a PvP arena"] = "Mostrar el marco mientras estás en una arena JcJ."
-L["Battleground"] = "Campo de batalla"
-L["Show the display while you are in a PvP battleground"] = "Mostrar el marco mientras estás en un campo de batalla JcJ."
+	["Overwritten"] = "Sobrescrito",
+	["Set the color for the target name when your %s has been overwritten."] = "Ajustar el color del nombre objetivo, mientras que tu %s se ha sobrescrito.",
 
-L["Exceptions"] = "Excepciones"
-L["Dead"] = "Muerto"
-L["Hide the display while you are dead"] = "Ocultar el marco mientras estás muerto."
-L["Out Of Combat"] = "Fuera de combate"
-L["Hide the display while you are out of combat"] = "Ocultar el marco mientras estás fuera de combate."
-L["Resting"] = "Reposo"
-L["Hide the display while you are in an inn or major city"] = "Ocultar el marco mientras estás en una fonda o gran ciudad (reposo)."
-L["Vehicle"] = "Vehículo"
-L["Hide the display while you are controlling a vehicle"] = "Ocultar el marco mientras estás manejando un vehículo."
+	["Inactive"] = "Inactivo",
+	["Set the color for expired, dispelled, or otherwise inactive shields."] = "Ajustar el color de los números de cargas, mientras que tu Escudo está inactivo.",
+
+	["Colorblind Mode"] = USE_COLORBLIND_MODE, -- Leave this as-is unless there is something wrong with Blizzard's translation in your locale
+	["Add asterisks around the target name when your %s has been overwritten, in addition to changing the color."] = "Añadir asteriscos alrededor del nombre objetivo cuando otro chamán sobrescribe tu %s, además de cambiar el color.",
 
 ------------------------------------------------------------------------
+
+	["Alerts"] = "Alertas",
+	["Use these settings to configure how ShieldsUp alerts you when a shield expires or is removed."] = "Estas opciones te permiten cambiar la forma en ShieldsUp te avisa en un Escudo expira o se quita.",
+
+	["Text Alert"] = "Alertas de texto",
+	["Show a text message when %s expires."] = "Mostrar un mensaje de alerta cuando expira tu %s.",
+
+	["Sound Alert"] = "Alertas de sonido",
+	["Play a sound when %s expires."] = "Reproducir un sonido cuando expira tu %s.",
+
+	["Sound File"] = "Sonido",
+	["Select the sound to play when %s expires."] = "Establecer el sonido a reproducir cuando expira su %s.",
+
+	["Text Output"] = "Salida de alertas de texto",
+
+------------------------------------------------------------------------
+
+	["Visibility"] = "Visibilidad",
+	["Use these settings to control when ShieldsUp should be shown or hidden."] = "Estas opciones le permiten controlar cuando el marco de ShieldsUp se muestran o se ocultan.",
+	["Enable"] = "Activar",
+
+	["Group Size"] = "Tamaño de grupo",
+--	["Solo"] = "",
+	["Show the display while you are not in a group"] = "Mostrar el marco mientras estás solo.",
+	["Party"] = "Grupo",
+	["Show the display while you are in a party group"] = "Mostrar el marco mientras estás en un grupo de 5.",
+	["Raid"] = "Banda",
+	["Show the display while you are in a raid group"] = "Mostrar el marco mientras estás en un banda.",
+
+	["Zone Type"] = "Tipo de Zona",
+	["World"] = "Mundo",
+	["Show the display while you are in the outdoor world"] = "Mostrar el marco mientras estás en el mundo, no una mazmorra.",
+	["Dungeon"] = "Mazmorra",
+	["Show the display while you are in a party dungeon"] = "Mostrar el marco mientras estás en una mazmorra.",
+	["Raid Dungeon"] = "Mazmorra de banda",
+	["Show the display while you are in a raid dungeon"] = "Mostrar el marco mientras estás en una mazmorra de banda.",
+--	["Arena"] = "",
+	["Show the display while you are in a PvP arena"] = "Mostrar el marco mientras estás en una arena JcJ.",
+	["Battleground"] = "Campo de batalla",
+	["Show the display while you are in a PvP battleground"] = "Mostrar el marco mientras estás en un campo de batalla JcJ.",
+
+	["Exceptions"] = "Excepciones",
+	["Dead"] = "Muerto",
+	["Hide the display while you are dead"] = "Ocultar el marco mientras estás muerto.",
+	["Out Of Combat"] = "Fuera de combate",
+	["Hide the display while you are out of combat"] = "Ocultar el marco mientras estás fuera de combate.",
+	["Resting"] = "Reposo",
+	["Hide the display while you are in an inn or major city"] = "Ocultar el marco mientras estás en una fonda o gran ciudad (reposo).",
+	["Vehicle"] = "Vehículo",
+	["Hide the display while you are controlling a vehicle"] = "Ocultar el marco mientras estás manejando un vehículo.",
+
+------------------------------------------------------------------------
+
+}
